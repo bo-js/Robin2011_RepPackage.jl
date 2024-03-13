4aaedadcad152980299f24e98d32609a5b1698c5
Documenter.jl <documenter@juliadocs.github.io>
2024-03-13 12:51:51 +0000

    build based on 4f54525

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Robin2011_RepPackage","category":"page"},{"location":"#Robin2011_RepPackage","page":"Home","title":"Robin2011_RepPackage","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Robin2011_RepPackage.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Robin2011_RepPackage]","category":"page"}]
+}
