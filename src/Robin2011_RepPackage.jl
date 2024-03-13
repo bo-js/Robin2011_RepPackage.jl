9b6b0be9e03f8d5e23f6a0b8f93cc7341ef19ceb
lewinno <lewin.nolden@sciencespo.fr>
2024-03-13 18:17:20 +0100

    testing value function iteration

--- src/Robin2011_RepPackage.jl
+++ src/Robin2011_RepPackage.jl
@@ -1,5 +1,37 @@
 module Robin2011_RepPackage
 
-# Write your package code here.
+using LinearAlgebra
+using Distributions
+
+function matchprod(x::Vector, y::Vector; B::Number = 1, C::Number = 0.7258)
+    return y * (B * x .+ C)'
+end
+
+function homeprod(x::Vector, y::Vector;  B::Number = 1, C::Number = 0.7258, α::Number = 0.75, zθ::Number = 0.7668)
+    return zθ .+ α * (matchprod(x,y) .- zθ)
+end
+
+
+function SurplusVFI(p::Matrix, z::Matrix, Π::Matrix; β::Number = 0.9466)
+    S =(I(length(Π[1,:])) - (β * Π))\(p - z) 
+    e = norm(S - max.(S,0),2)
+
+    while e > 0.00001
+        S1 = S
+        S = p - z + β * Π * max.(S,0)
+        e = norm(S - S1, 2)
+    end
+
+    return S 
+
+end
+
+
+
+export matchprod
+export homeprod
+
+
+
 
 end
