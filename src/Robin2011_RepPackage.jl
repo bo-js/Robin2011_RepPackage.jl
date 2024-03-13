4b77124105d6db9af519d70eb228f6675c8f86ab
Bo Jacobs Strom <bojs.13@gmail.com>
2024-03-13 18:23:24 +0100

    Added SurplusVFI function and script for Grid Initialisation

--- src/Robin2011_RepPackage.jl
+++ src/Robin2011_RepPackage.jl
@@ -1,5 +1,32 @@
 module Robin2011_RepPackage
 
-# Write your package code here.
+using LinearAlgebra
+using Distributions
+
+function matchprod(x::Vector, y::Vector; B::Number = 1, C::Number = 0.725767358913686)
+    return y * (B * x .+ C)'
+end
+
+function homeprod(x::Vector, y::Vector; B::Number = 1, C::Number = 0.725767358913686, α::Number = 0.75, z0::Number = 0.766752794650811)
+    return z0 .+ α * (matchprod(x, y; B, C) .- z0)
+end
+
+function SurplusVFI(p::Matrix, z::Matrix, Π::Matrix; β::Number = 0.946603693905558)
+    S = (I(length(Π[1,:])) - β * Π )\(p - z)
+    e = norm(S - max.(S, 0), 2)
+
+    while e > 0.00001
+        S1 = S
+        S = p - z + β * Π * max.(S, 0)
+        e = norm(S - S1, 2)
+    end
+
+    return S
+end
+
+
+export SurplusVFI
+export matchprod
+export homeprod
 
 end
---
+++ src/grids.jl
@@ -0,0 +1,19 @@
+using Distributions
+using BivariateCopulas
+
+trim = 0.002
+M = 500
+N = 100
+σ = 0.0257
+
+x = collect(LinRange(trim, 1 - trim, M))
+F = collect(LinRange(trim, 1- trim, N))
+y = exp.(σ * quantile(Normal(), F))
+
+ρ = 0.913702234286476
+
+cop = Gaussian(ρ)
+
+P = [density(cop, F[i], F[j]) for i in 1:N, j in 1:N ]
+
+Π = [P[i, j]/sum(P[i, :]) for i in 1:N, j in 1:N]
