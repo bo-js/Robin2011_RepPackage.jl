eb0bec772ed23dfb7554643b1e561ddfd466d986
Bo Jacobs Strom <bojs.13@gmail.com>
2024-03-15 15:48:32 +0100

    added comments for unemp_path function

--- src/dynamics.jl
+++ src/dynamics.jl
@@ -1,10 +1,12 @@
-function unemp_path(S::Matrix, yt::Vector, T::Integer; λ0::Number = 0.994544861919718, δ::Number = 0.041563759920623)
+function unemp_path(S::Matrix, statet::Vector, T::Integer; λ0::Number = 0.994544861919718, δ::Number = 0.041563759920623)
     M = length(S[1, :])
 
     ut = zeros(T, M)
 
-    ut[1, :] = [(δ/(δ + λ0))*(S[yt[t], m] > 0) + 1 - (S[yt[t], m] > 0) for m in 1:M]
+    ## First Period Starts at Steady State
+    ut[1, :] = [(δ/(δ + λ0))*(S[statet[t], m] > 0) + 1 - (S[statet[t], m] > 0) for m in 1:M]
 
-    ut[2:T, :] = [1 - ((1 - δ)*(1 - ut[t-1, m]) + λ0 * ut[t-1, m])*(S[yt[t-1], m] > 0) for t in 2:T, m in 1:M]
+    ## Define Further Periods Recursively
+    ut[2:T, :] = [1 - ((1 - δ)*(1 - ut[t-1, m]) + λ0 * ut[t-1, m])*(S[statet[t-1], m] > 0) for t in 2:T, m in 1:M]
 
 end
