b8d1dd3b3a72f1936892f709ac868cdc9737ff7c
Bo Jacobs Strom <bojs.13@gmail.com>
2024-05-04 15:58:38 +0200

    fixed wages, added optimization

--- src/wages.jl
+++ src/wages.jl
@@ -1,4 +1,4 @@
-function WageVFI(S::Matrix, Π::Matrix, z::Matrix; λ1::Number = 0.119345383430366, β::Number = 0.946603693905558)
+function WageVFI(S::Matrix, Π::Matrix, z::Matrix; λ1 = 0.119345383430366, β::Number = 0.946603693905558)
     
     N = length(Π[1, :])
     M = length(S[1, :])
@@ -7,46 +7,47 @@ function WageVFI(S::Matrix, Π::Matrix, z::Matrix; λ1::Number = 0.1193453834303
     Wmax = zeros(N, N, M)
     wmin = zeros(N, M)
     wmax = zeros(N, M)
-
-    for m in 1:length(M)
-        for i in 1:length(N)
+    kf = zeros(N)
+    kf .= λ1
+    for m in 1:M
+        for i in 1:N
 
             # Minimum Wages - W is a vector giving the worker surplus of being in each state with the minimum wage set for state i
             A = (Π - repeat(Π[i, :]', N, 1)) * repeat((S[:, m] .> 0)', N, 1)
-            W = (I(length(Π[1, :])) - β * A * (1 - λ1)) \ (z[i, m] .- z[:, m] + β * A * λ1 * S[:, m])
+            W = (I(length(Π[1, :])) - β * A * repeat((1 .- kf)', N, 1)) \ (z[i, m] .- z[:, m] + β * A *( kf .* S[:, m]))
             W0 = min.(max.(W, 0), S[:, m])
             e = norm(W - W0, 2)
 
-            while e > 0.00001
+            while e > 0.001
                 W1 = W
-                W = z[i, m] .- z[:, m] + β * A * (λ1 * S[:, m] + (1 - λ1) * W0)
+                W = z[i, m] .- z[:, m] + β * A * (kf .* S[:, m] + (1 .- kf) .* W0)
                 W0 = min.(max.(W, 0), S[:, m])
                 e = norm(W - W1, 2)
             end
 
             Wmin[:, i, m] = W
-            wmin[i, m] = z[i, m] - β * Π[i, :]'*(S[:, m] .> 0)
+            wmin[i, m] = z[i, m] - first(β * Π[i, :]' .* (S[:, m] .> 0)' * (kf .* S[:, m] + (1 .- kf) .* W0))
 
 
             ## Maximum Wages - Likewise as above
 
-            W = (I(length(Π[1, :])) - β * A * (1 - λ1)) \ (S[i, m] + z[i, m] .- z[:, m] + β * A * λ1 * S[:, m])
+            W = (I(length(Π[1, :])) - β * A * repeat((1 .- kf)', N, 1)) \ (S[i, m] + z[i, m] .- z[:, m] + β * A * (kf .* S[:, m]))
             W0 = min.(max.(W, 0), S[:, m])
             e = norm(W - W0, 2)
 
-            while e > 0.00001
+            while e > 0.001
                 W1 = W
-                W = S[i, m] + z[i, m] .- z[:, m] + β * A * (λ1 * S[:, m] + (1 - λ1) * W0)
+                W = S[i, m] + z[i, m] .- z[:, m] + β * A * (kf .* S[:, m] + (1 .- kf) .* W0)
                 W0 = min.(max.(W, 0), S[:, m])
                 e = norm(W - W1, 2)
             end
 
             Wmax[:, i, m] = W
-            wmax[i, m] = S[i, m] + z[i, m] - β * Π[i, :]'*(S[:, m] .> 0)
+            wmax[i, m] = S[i, m] + z[i, m] - first(β * Π[i, :]' .* (S[:, m] .> 0)' * (kf .* S[:, m] + (1 .- kf) .* W0))
 
 
         end
     end
-
+ 
     return Dict(:Wmin => Wmin, :wmin => wmin, :Wmax => Wmax, :wmax => wmax)
 end
