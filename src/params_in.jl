ceffdc637aebb51512688fecc07fb0464badacdf
Bo Jacobs Strom <bojs.13@gmail.com>
2024-05-12 17:53:46 +0200

    added more docstrings

--- src/params_in.jl
+++ src/params_in.jl
@@ -1,3 +1,27 @@
+"""
+`params_estimated(; path = "x1.txt")`
+
+This function outputs all necessary parameters for the model, from a set of estimated parameters.
+
+As it's input it takes a file path, which must point to a `.txt` file containing the parameter values correctly ordered, line separated, and transformed as in `estmain.jl`. 
+
+The function then performs the appropriate inverse transformations and returns the parameters as a `NamedTuple`. 
+
+The default file path points to those estimated by `estmain.jl`, stored in `x1.txt`. Alternatively to use those from Robin(2011), replace this with `x0.txt`. 
+
+The parameters must be stored in the text file in the following order with the following transformations.
+
+- `log(ν)`
+- `log(μ)`
+- `loginv(δ)`
+- `loginv(λ0)`
+- `atanh(ρ)`
+- `log(σ)`
+- `log(z0)`
+- `log(C)`
+
+Where `logit = x -> 1/(1+exp(x))` and `loginv = x -> log(1/x - 1)`.
+"""
 function params_estimated(; path = "x1.txt")
     b = parse.(Float64, readlines(path))
     
