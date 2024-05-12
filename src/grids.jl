ccb399d9b53bdaf4e5b867446ec0f21d88dda65b
Bo Jacobs Strom <bojs.13@gmail.com>
2024-05-12 19:33:12 +0200

    improved grids docs

--- src/grids.jl
+++ src/grids.jl
@@ -1,9 +1,14 @@
 export grids
 
 """
-grids(; M = 500, N = 100, ρ = 0.913702234286476, σ = 0.0257,ν::Number = 2.019365636076711, μ::Number = 5.786082109731152 )
+`grids(; M = 500, N = 100, ρ = 0.913702234286476, σ = 0.0257,ν::Number = 2.019365636076711, μ::Number = 5.786082109731152 )`
 
-This function takes parameter values and constructs the grids and distributions of worker types and aggregate shocks, which it returns in a Dict.
+This function takes parameter values and constructs the grids and distributions of worker types and aggregate shocks, which it returns in a Dict, the Dict keys are:
+- `:x`, the grid of worker productivities
+- `:y`, the grid of aggregate shocks,
+- `:Π`, the Markov transition matrix,
+- `:l`, the pdf of worker types,
+- `:F`, the grid of aggregate shocks before transformed to fit the estimated distribution.
 
 """
 function grids(; M = 500, N = 100, ρ = 0.913702234286476, σ = 0.0257,ν::Number = 2.019365636076711, μ::Number = 5.786082109731152 )
